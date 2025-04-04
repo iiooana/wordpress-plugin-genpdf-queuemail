3275682e2b095976d01135455e549270301cf54a
iiooana <ioana2502@yahoo.it>
2025-04-04 18:12:19 +0200

    init

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+/*
+ * Plugin Name: Generate PDF
+ * Description: Generate PDF from orders and send emails in a queue.
+ * Version: 0.0.1
+ * Requires at least: 6.0
+ * Requires PHP: 8.3
+ * Author: Ioana
+ */
