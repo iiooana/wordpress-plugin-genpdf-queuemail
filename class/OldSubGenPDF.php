78029a49a1a8b15d71b150a9079ec83a0d9914fb
Ioana <ioana2502@yahoo.it>
2025-04-15 14:07:17 +0200

    add query prepare to avoid sql injection

--- class/OldSubGenPDF.php
+++ class/OldSubGenPDF.php
@@ -13,29 +13,42 @@ class OldSubGenPDF {
     }
 
     public function getCount(){
-        return $this->db->get_var("select count(*) from {$this->db->base_prefix}vxcf_leads");
+        $query_prep = $this->db->prepare(
+            "select count(*) from {$this->db->base_prefix}vxcf_leads"
+        );
+        return $this->db->get_var($query_prep);
     }
 
     public function getData($page = 1){
-        return $this->db->get_results("
-        SELECT id,url,created
-        FROM {$this->db->base_prefix}vxcf_leads as leads 
-        order by id desc
-        limit {$this->count_per_page}
-        ",ARRAY_A);
+        $query_prep =  $this->db->prepare(
+            "SELECT id,url,created
+            FROM {$this->db->base_prefix}vxcf_leads as leads 
+            order by id desc
+            limit {$this->count_per_page}"
+            );
+        return $this->db->get_results($query_prep, ARRAY_A);
        // genpdf_vardie($test);
     }
 
     public function getShortDetail($lead_id){  
+        $query_nome = $this->db->prepare(
+            "SELECT value from {$this->db->base_prefix}vxcf_leads_detail where lead_id=%d and name=%s limit 1",
+            [$lead_id,'nome']
+        );
+        $query_cognome = $this->db->prepare(
+            "SELECT value FROM {$this->db->base_prefix}vxcf_leads_detail where lead_id=%d and name=%s limit 1",
+            [$lead_id,'cognome']
+        );
         return [
-           "nome" => $this->db->get_results("SELECT value from {$this->db->base_prefix}vxcf_leads_detail where lead_id={$lead_id} and name='nome' limit 1 ",ARRAY_A)[0]['value'],
-           "cognome" => $this->db->get_results("SELECT value from {$this->db->base_prefix}vxcf_leads_detail where lead_id={$lead_id} and name='cognome' limit 1 ",ARRAY_A)[0]['value']
+           "nome" => $this->db->get_row($query_nome,ARRAY_A)['value'],
+           "cognome" => $this->db->get_row($query_cognome,ARRAY_A)['value']
         ];
     }
     public function getDetail($lead_id){
-        return $this->db->get_results(" SELECT * from  {$this->db->base_prefix}vxcf_leads_detail where lead_id={$lead_id} order by id asc ",ARRAY_A);
+        $query = $this->db->prepare("SELECT * from {$this->db->base_prefix}vxcf_leads_detail where lead_id=%d order by id asc",[$lead_id]);
+        return $this->db->get_results($query,ARRAY_A);
     }
-    
+
 }
 
 ?>
--- views/old_subscritions.php
+++ views/old_subscritions.php
@@ -3,52 +3,75 @@
 } ?>
 <div class="wrap">
     <h1 class="wp-heading-inline"><?= __('Subscriptions received from the old website', 'genpdf-woocommerce') ?></h1>
-    <div class="tablenav top">
-        <div class="alignleft actions bulkactions">
+    <? if (!empty($_GET['page']) && $_GET['page'] == 'genpdf_data' && !empty($_GET['lead_id']) && is_numeric($_GET['lead_id'])) { ?>
+        <table class="wp-list-table widefat fixed striped pages">
+            <? $detail = $subs->getDetail($_GET['lead_id']);
+            if (!empty($detail)) {  ?>
+                <tbody>
+                    <? foreach($detail as $item){ ?>
+                        <tr>
+                            <td><?=!empty($item['name']) ? strtoupper($item['name']) : ''?></td>
+                            <td><strong><?=$item['value']?></strong></td>
+                        </tr>
+                    <? } ?>
+                </tbody>
+            <? } ?>
+        </table>
+
+        <table>
+            
+        </table>
+
+    <? } else { ?>
+        <div class="tablenav top">
+            <div class="alignleft actions bulkactions">
+            </div>
+            <div class="alignleft actions">
+            </div>
+            <div class="tablenav-pages"><span class="displaying-num"><?= $subs->getCount() ?> <?= __('items', 'genpdf-woocommerce') ?></span>
+                <span class="pagination-links"><span class="tablenav-pages-navspan button disabled" aria-hidden="true">«</span>
+                    <span class="tablenav-pages-navspan button disabled" aria-hidden="true">‹</span>
+                    <span class="paging-input"><label for="current-page-selector" class="screen-reader-text">Current Page</label><input class="current-page" id="current-page-selector" type="text" name="paged" value="1" size="1" aria-describedby="table-paging"><span class="tablenav-paging-text"> of <span class="total-pages">2</span></span></span>
+                    <a class="next-page button" href="http://localhost:8081/wp-admin/edit.php?post_type=page&amp;paged=2"><span class="screen-reader-text">Next page</span><span aria-hidden="true">›</span></a>
+                    <span class="tablenav-pages-navspan button disabled" aria-hidden="true">»</span></span>
+            </div>
+            <br class="clear">
         </div>
-        <div class="alignleft actions">
-        </div>
-        <div class="tablenav-pages"><span class="displaying-num"><?= $subs->getCount() ?> <?= __('items', 'genpdf-woocommerce') ?></span>
-            <span class="pagination-links"><span class="tablenav-pages-navspan button disabled" aria-hidden="true">«</span>
-                <span class="tablenav-pages-navspan button disabled" aria-hidden="true">‹</span>
-                <span class="paging-input"><label for="current-page-selector" class="screen-reader-text">Current Page</label><input class="current-page" id="current-page-selector" type="text" name="paged" value="1" size="1" aria-describedby="table-paging"><span class="tablenav-paging-text"> of <span class="total-pages">2</span></span></span>
-                <a class="next-page button" href="http://localhost:8081/wp-admin/edit.php?post_type=page&amp;paged=2"><span class="screen-reader-text">Next page</span><span aria-hidden="true">›</span></a>
-                <span class="tablenav-pages-navspan button disabled" aria-hidden="true">»</span></span>
-        </div>
-        <br class="clear">
-    </div>
-    <table class="wp-list-table widefat fixed striped pages">
-        <thead>
-            <tr>
+        <table class="wp-list-table widefat fixed striped pages">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th><?= __('First name', 'genpdf-woocommerce') ?></th>
+                    <th><?= __('Last name', 'genpdf-woocommerce') ?></th>
+                    <th>URL</th>
+                    <th><?= __('Created at', 'genpdf-woocommerce') ?></th>
+                </tr>
+            </thead>
+            <tbody>
+                <? $data  = $subs->getData(); ?>
+                <? if (!empty($data) && count($data) > 0) {
+                    foreach ($data as $item) {
+                        $detail = $subs->getShortDetail($item['id']);
+                ?>
+                        <tr>
+                            <td> <?= $item['id'] ?> <a href="?page=genpdf_data&lead_id=<?= $item['id'] ?>"> <?= __('Visualizza', 'genpdf-woocommerce') ?> </a></td>
+                            <td> <?= $detail['nome'] ?> </td>
+                            <td> <?= $detail['cognome'] ?> </td>
+                            <td><a href="<?= $item['url'] ?>" target="_blank"><?= $item['url'] ?></a></td>
+                            <td> <?= !empty($item['created']) ? date('H:i d/m/Y', strtotime($item['created'])) : '' ?> </td>
+                        </tr>
+                <? }
+                } ?>
+            </tbody>
+            <tfoot>
                 <th>ID</th>
                 <th><?= __('First name', 'genpdf-woocommerce') ?></th>
                 <th><?= __('Last name', 'genpdf-woocommerce') ?></th>
                 <th>URL</th>
                 <th><?= __('Created at', 'genpdf-woocommerce') ?></th>
-            </tr>
-        </thead>
-        <tbody>
-            <? $data  = $subs->getData(); ?>
-            <? if(!empty($data) && count($data) > 0){ 
-                foreach($data as $item ){
-                    $detail = $subs->getShortDetail($item['id']);
-                    ?>
-                    <tr>
-                        <td><?=$item['id']?></td>
-                        <td> <?=$detail['nome']?> </td>
-                        <td> <?=$detail['cognome']?> </td>
-                        <td><a href="<?=$item['url']?>" target="_blank"><?=$item['url']?></a></td>
-                        <td> <?= !empty($item['created']) ? date('H:i d/m/Y',strtotime($item['created'])) : ''?> </td>
-                    </tr>
-               <? }
-             } ?>
-        </tbody>
-        <tfoot>
-            <th>ID</th>
-            <th><?= __('First name', 'genpdf-woocommerce') ?></th>
-            <th><?= __('Last name', 'genpdf-woocommerce') ?></th>
-            <th>URL</th>
-            <th><?= __('Created at', 'genpdf-woocommerce') ?></th>
-        </tfoot>
-    </table>
+            </tfoot>
+        </table>
+    <? }  ?>
+
+
 </div>
