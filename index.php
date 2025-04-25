db7770ca30e10dbfe9652af25bc5d5870dd259a4
Ioana <ioana2502@yahoo.it>
2025-04-25 13:44:54 +0200

    add more metadata of products related to the order

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 /*
  * Plugin Name: Generate PDF
  * Description: Generate a PDF compiled of the order's data. View the history data of the "Contact Form Entries" plugin. 
- * Version: 0.2.3
+ * Version: 0.3.0
  * Author: Ioana
  * Text Domain: genpdf-woocommerce
  * Domain Path: /languages
@@ -32,7 +32,6 @@ function genpdf_getPath()
     return  plugin_dir_path(__FILE__);
 }
 
-
 register_activation_hook(__FILE__, 'genpdf_active');
 
 /**
@@ -55,125 +54,87 @@ function add_extra_order_meta($order_id)
     $order = wc_get_order($order_id);
     //get the products of the order
     $products = $order->get_items();
-    //on the website it is possbile to buy only one product at time.
-    if (count($products) > 1) {
-        $message = var_export(['message' => 'The order has more than one product, this is a logical error.', 'order_id' => $order_id], true);
-        error_log($message);
-    }
 
     //region Save order_metadata
     if (!empty($products)) {
         foreach ($products as $item) {
-            //genpdf_vardie($item);
-            $product = $item->get_product();
-            if (!empty($product) && !empty($product->get_parent_id())) {              
-                //genpdf_vardie($product->attributes);
-                
+            $product = $item->get_product(); //this is a product variantion
+            if (!empty($product) && !empty($product->get_parent_id())) {
+                $array_product_metadata = [];
+                $array_product_metadata['product_id'] = $product->get_id();               
+
                 //region giorno_generico_settimana
-                if( !empty($product->attributes['pa_data']) ){
-                    $wpdb->insert(
-                        $wpdb->base_prefix . 'wc_orders_meta',
-                        [
-                            "order_id" => $order_id,
-                            "meta_key" => "giorno_generico_settimana",
-                            "meta_value" => $product->attributes['pa_data']
-                        ],
-                        ['%d', '%s', '%s']);
+                if (!empty($product->attributes['pa_data'])) {
+                    $array_product_metadata['giorno_generico_settimana'] = $product->attributes['pa_data'];
                 }
                 //endregion
-
                 //region acconto or totale
-                if( !empty( $product->attributes['pa_pagamento'] ) ){
-                    $wpdb->insert(
-                        $wpdb->base_prefix . 'wc_orders_meta',
-                        [
-                            "order_id" => $order_id,
-                            "meta_key" => "acconto_o_totale",
-                            "meta_value" => $product->attributes['pa_pagamento']
-                        ],
-                        ['%d', '%s', '%s']);    
+                if (!empty($product->attributes['pa_pagamento'])) {
+                    $array_product_metadata['acconto_o_totale'] = $product->attributes['pa_pagamento'];
                 }
                 //endregion
 
                 $id_product = $product->get_parent_id();
-
                 if (!empty($id_product)) {
+                    $array_product_metadata['parent_product_id'] = $id_product;
                     //region set the template
                     $template_id = TemplateGenPDF::getIdTemplateByProduct($id_product);
-                    if(!empty($template_id)){
-                      $wpdb->insert(
-                            $wpdb->base_prefix."genpdf_orders_template",
+                    if (!empty($template_id)) {
+                        $wpdb->insert(
+                            $wpdb->base_prefix . "genpdf_orders_template",
                             [
                                 "order_id" => $order_id,
                                 "template_id" => $template_id
-                            ],['%d','%d']);                            
-                    }else{
-                        $message = var_export(['message'=>'Errore non trovo il template per product_id.','product_id' => $id_product],true);
+                            ],
+                            ['%d', '%d']
+                        );
+                    } else {
+                        $message = var_export(['message' => 'Errore non trovo il template per product_id.', 'product_id' => $id_product], true);
                         error_log($message);
-                    }                  
-                    //endregion
-
-                    //region luogo
-                    $meta_value = get_field('luogoluoghi_corso', $id_product);
-                    $wpdb->insert(
-                        $wpdb->base_prefix . 'wc_orders_meta',
-                        [
-                            "order_id" => $order_id,
-                            "meta_key" => "luogo_del_corso",
-                            "meta_value" => $meta_value
-                        ],
-                        ['%d', '%s', '%s']);
-                    //endregion
-
-                    //region anno accademico
-                    $meta_value = get_field('anno_accademico', $id_product);
-                    $wpdb->insert(
-                        $wpdb->base_prefix . 'wc_orders_meta',
-                        [
-                            "order_id" => $order_id,
-                            "meta_key" => "anno_accademico",
-                            "meta_value" => $meta_value
-                        ],
-                        ['%d', '%s', '%s']);
-                    //endregion
-
-                    //region importi mensili
-                    $tabella_importi_prodotto = get_field('tabella_importo_mese', $id_product);
-                    $mesi = [];
-                    if (!empty($tabella_importi_prodotto)) {
-                        //genpdf_vardie($tabella_importi_prodotto);
-                        foreach ($tabella_importi_prodotto as $key => $value) {
-                            //genpdf_vardie($key);
-                            if (!empty($value) && floatval($value) > 0) {
-                                match ($key) {
-                                    "importo_gennaio" => $mesi['gennaio'] = $value,
-                                    "importo_febbrario" => $mesi['febbraio'] = $value,
-                                    "importo_marzo" => $mesi['marzo'] = $value,
-                                    "importo_aprile" => $mesi['aprile'] = $value,
-                                    "importo_maggio" => $mesi['maggio'] = $value,
-                                    "importo_giugno" => $mesi['giugno'] = $value,
-                                    "importo_luglio" => $mesi['luglio'] = $value,
-                                    "importo_agosto" => $mesi['agosto'] = $value,
-                                    "importo_settembre" => $mesi['settembre'] = $value,
-                                    "importo_ottobre" => $mesi['ottobre'] = $value,
-                                    "importo_novembre" => $mesi['novembre'] = $value,
-                                    "importo_dicembre" => $mesi['dicembre'] = $value
-                                };
-                            }
-                        }
                     }
+                    //endregion
 
+                    //region product field
+                    $array_product_metadata['crediti_ecm'] = get_field('crediti_ecm', $id_product);
+                    $array_product_metadata['luogo_del_corso'] = get_field('luogoluoghi_corso', $id_product);
+                    $array_product_metadata['anno_accademico'] = get_field('anno_accademico', $id_product);
+                    $array_product_metadata['titolo_corso_pdf'] = get_field('titolo_corso_pdf', $id_product);
+                        //region importi mensili
+                        $tabella_importi_prodotto = get_field('tabella_importo_mese', $id_product);
+                        if (!empty($tabella_importi_prodotto)) {
+                            foreach ($tabella_importi_prodotto as $key => $value) {
+                                if (!empty($value) && floatval($value) > 0) {
+                                    match ($key) {
+                                        "importo_gennaio" => $array_product_metadata['importo_mese']['gennaio'] = $value,
+                                        "importo_febbrario" => $array_product_metadata['importo_mese']['febbraio'] = $value,
+                                        "importo_marzo" => $array_product_metadata['importo_mese']['marzo'] = $value,
+                                        "importo_aprile" => $array_product_metadata['importo_mese']['aprile'] = $value,
+                                        "importo_maggio" => $array_product_metadata['importo_mese']['maggio'] = $value,
+                                        "importo_giugno" => $array_product_metadata['importo_mese']['giugno'] = $value,
+                                        "importo_luglio" => $array_product_metadata['importo_mese']['luglio'] = $value,
+                                        "importo_agosto" => $array_product_metadata['importo_mese']['agosto'] = $value,
+                                        "importo_settembre" => $array_product_metadata['importo_mese']['settembre'] = $value,
+                                        "importo_ottobre" => $array_product_metadata['importo_mese']['ottobre'] = $value,
+                                        "importo_novembre" => $array_product_metadata['importo_mese']['novembre'] = $value,
+                                        "importo_dicembre" => $array_product_metadata['importo_mese']['dicembre'] = $value
+                                    };
+                                }
+                            }                        
+                        }                    
+                        //endregion
+                    //endregion                
+                    
+                    //region insert to db
                     $wpdb->insert(
                         $wpdb->base_prefix . 'wc_orders_meta',
                         [
                             "order_id" => $order_id,
-                            "meta_key" => "importo_mese",
-                            "meta_value" => json_encode($mesi)
+                            "meta_key" => "product_detail",
+                            "meta_value" => json_encode($array_product_metadata)
                         ],
                         ['%d', '%s', '%s']
                     );
                     //endregion
-
                 }
             }
         }
