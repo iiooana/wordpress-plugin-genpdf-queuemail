b180effe726a56b02192a07c38b0b82d863200ed
Ioana <ioana2502@yahoo.it>
2025-04-30 17:24:55 +0200

    update version

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 /*
  * Plugin Name: Generate PDF
  * Description: Generate a PDF compiled of the order's data. View the history data of the "Contact Form Entries" plugin. 
- * Version: 0.3.9
+ * Version: 0.4.0
  * Author: Ioana
  * Text Domain: genpdf-woocommerce
  * Domain Path: /languages
