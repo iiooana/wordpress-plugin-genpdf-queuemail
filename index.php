8bbe75967ba7fc5593359436b4b77d5a06bd724f
Ioana <ioana2502@yahoo.it>
2025-05-14 16:22:38 +0200

    update version

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 /*
  * Plugin Name: Generate PDF
  * Description: Generete PDF from customer data and product data. Manage the queue of emails with attachments.
- * Version: 0.4.2
+ * Version: 0.4.3
  * Author: Ioana
  * Text Domain: genpdf-woocommerce
  * Domain Path: /languages
