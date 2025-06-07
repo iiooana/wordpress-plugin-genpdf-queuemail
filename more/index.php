f732db2da8e8525852a3b258e25d692a897289d3
Ioana <ioana2502@yahoo.it>
2025-06-07 10:46:15 +0200

    change settings plugin

--- more/index.php
+++ more/index.php
@@ -57,3 +57,52 @@ function DSCFW_product_get_data($post){
 		update_post_meta($signature_imgid, '_wp_attachment_image_alt', "Frima ordine #".$post);
 	}
 }
+//disable signature on email
+/* Display Signature Thankyou Page */
+/*$signature_order_position = get_option('signature_order_position','inside_order_detail');
+
+if($signature_order_position == 'inside_order_detail'){
+	add_action('woocommerce_order_details_after_order_table_items', 'DSCFW_order_details');
+}else{
+	add_action('woocommerce_order_details_after_customer_details', 'DSCFW_order_details');
+}
+function DSCFW_order_details($order){
+	$post_attach_file = get_post_meta($order->get_id(),'signpad',true);
+	$signimage = wp_get_attachment_url($post_attach_file, true);
+	if(!empty($signimage)){
+	?>
+	<section class="woocommerce-customer-details mycustomsection">
+		<div class="woocommerce-order-details__title">
+			<h2 class="signatureheading"><?php echo esc_html('Your Signature','digital-signature-checkout-for-woocommerce-pro'); ?></h2>
+			<img src="<?Php echo esc_url($signimage); ?>" width="200" height="300">
+		</div>
+	</section>
+	<?php
+	}
+}
+
+
+add_action( 'woocommerce_admin_order_data_after_billing_address', 'DSCFW_checkout_field_display_admin_order_meta', 10, 1 );
+function DSCFW_checkout_field_display_admin_order_meta($order){
+	// $post_attach_file = $order->get_meta( 'signpad' );
+	$post_attach_file = get_post_meta($order->get_id(),'signpad',true);
+	$signimage = wp_get_attachment_url($post_attach_file, true);
+	if(!empty($signimage)){
+    ?>
+    	<img src="<?Php echo esc_url($signimage); ?>" width="100" height="100">
+    <?php
+	}
+}
+
+add_action( 'woocommerce_email_order_details', 'DSCFW_action_wc_email_order_details', 50, 4 );
+function DSCFW_action_wc_email_order_details( $order, $sent_to_admin, $plain_text, $email ){
+	// $post_attach_file = $order->get_meta( 'signpad' );
+	$post_attach_file = get_post_meta($order->get_id(),'signpad',true);
+	$signimage = wp_get_attachment_url($post_attach_file, true);
+	if(!empty($signimage)){
+    ?>
+        <h2 class="signatureheading"><?php echo esc_html('Your Signature','digital-signature-checkout-for-woocommerce-pro'); ?></h2>
+    	<img src="<?Php echo esc_url($signimage); ?>" width="100" height="100">
+    <?php
+	}
+}*/
